d30e831777cf95425274778adee016d3899620c0
Sarah Hanafy <sarahhanafy20@gmail.com>
2024-11-28 15:14:49 +0200

    [FEAT] add a debouncer fot the search

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,20 +1,69 @@
 import React from "react";
-import { TextField } from "@mui/material";
+import { IconButton, TextField } from "@mui/material";
 import { useAppStore } from "../../store/AppStore";
+import { useDebounce } from "../../hooks/useDebounce";
+import CloseIcon from '@mui/icons-material/Close';
 
 const SearchBar: React.FC = () => {
   const setSearch = useAppStore((state) => state.setSearch);
+  const [searchInput, setSearchInput] = React.useState("");
+  const debouncedSearch = useDebounce(searchInput, 300);
 
-  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setSearch(e.target.value);
+  React.useEffect(() => {
+    setSearch(debouncedSearch); 
+  }, [debouncedSearch, setSearch]);
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchInput(e.target.value);
   };
 
+  const handleClearSearch = () => {
+    setSearchInput("");
+    setSearch("");
+  };
+
+  
   return (
-    <TextField
+    <TextField    
       fullWidth
       placeholder="Search stocks..."
       variant="outlined"
-      onChange={handleSearchChange}
+      onChange={handleInputChange}
+      value={searchInput}
+      InputProps={{
+        endAdornment: searchInput && (
+          <IconButton
+            onClick={handleClearSearch}
+            edge="end"
+            sx={{ visibility: searchInput ? 'visible' : 'hidden' }}
+          >
+            <CloseIcon />
+          </IconButton>
+        ),
+      }}
+      sx={{
+        margin: '20px 0',
+        '& .MuiOutlinedInput-root': {
+          backgroundColor: '#ffffff', 
+          borderRadius: '8px',
+          '&:hover .MuiOutlinedInput-notchedOutline': {
+            borderColor: '#1976d2',
+          },
+          '&.Mui-focused .MuiOutlinedInput-notchedOutline': {
+            borderColor: '#1976d2',
+            borderWidth: '2px',
+          }
+        },
+        '& .MuiOutlinedInput-input': {
+          padding: '14px',
+          fontSize: '1rem',
+          '&::placeholder': {
+            color: '#666',
+            opacity: 0.8,
+          }
+        },
+        width: { xs: '100%', md: '50%' }
+      }}
     />
   );
 };
