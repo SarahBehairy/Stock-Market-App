a2ad358e26a13d42b68b14ea1239eba50bcaf0fd
Sarah Hanafy <sarahhanafy20@gmail.com>
2024-11-28 13:54:38 +0200

    [FEAT] add loading spinner component

---
+++ src/components/LoadingSpinner/LoadingSpinner.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import CircularProgress from "@mui/material/CircularProgress";
+
+const LoadingSpinner: React.FC = () => <CircularProgress />;
+
+export default LoadingSpinner;
