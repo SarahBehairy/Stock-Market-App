51fea6ab9698346217774cf7a12d5499cb56d841
Sarah Hanafy <sarahhanafy20@gmail.com>
2024-11-28 13:53:39 +0200

    [FEAT] add app store

---
+++ src/store/AppStore.ts
@@ -0,0 +1,17 @@
+import { create } from 'zustand'
+
+interface AppState {
+    search: string;
+    darkMode: boolean;
+    setSearch: (value: string) => void;
+    toggleDarkMode: () => void;
+  }
+  
+
+export const useAppStore = create<AppState>((set) => ({
+  search: "",
+  darkMode: false,
+  setSearch: (value: string) => set({ search: value }),
+  toggleDarkMode: () =>
+    set((state: { darkMode: boolean;}) => ({ darkMode: !state.darkMode })),
+}));
